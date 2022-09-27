b49bdb5d9ca25678ca00467d3886d98e1fbaa88b
Eric Neilsen <neilsen@fnal.gov>
2022-09-27 15:06:22 -0500

    Reorg of app code to support conda entry points

---
+++ schedview/app/start.py
@@ -0,0 +1,26 @@
+import bokeh.command.bootstrap
+from pathlib import Path
+
+
+def start_app(app_name):
+    """Start a bokeh app.
+
+    Parameters
+    ----------
+    app_name : `str`
+        The name of the bokeh app (and submodule of schedview.app)
+
+    """
+    base_dir = Path(__file__).resolve().parent
+    app_dir = Path(base_dir, app_name).as_posix()
+    bokeh.command.bootstrap.main(["bokeh", "serve", app_dir])
+
+
+def sched_maps():
+    """Start the sched_maps app."""
+    start_app("sched_maps")
+
+
+def metric_maps():
+    """Start the metric_maps app."""
+    start_app("metric_maps")
