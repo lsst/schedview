0c35f22323ed24093e51ba1dea7f10319075288b
Eric Neilsen <neilsen@fnal.gov>
2023-08-15 09:15:42 -0700

    black

--- schedview/plot/nightly.py
+++ schedview/plot/nightly.py
@@ -28,26 +28,66 @@ DEFAULT_EVENT_COLORS = {
     "night_middle": None,
 }
 
+
 def _visits_tooltips(visits, weather=False):
-    deg = u"\u00b0"
+    deg = "\u00b0"
     tooltips = [
-        ("Start time", "@start_date{%F %T} UTC (mjd=@observationStartMJD{00000.00}, LST=@observationStartLST"+deg+")"),
-        ('flush by mjd', '@flush_by_mjd{00000.00}'),
+        (
+            "Start time",
+            "@start_date{%F %T} UTC (mjd=@observationStartMJD{00000.00}, LST=@observationStartLST"
+            + deg
+            + ")",
+        ),
+        ("flush by mjd", "@flush_by_mjd{00000.00}"),
         ("Note", "@note"),
         ("Filter", "@filter"),
-        ("Field coordinates", "RA=@fieldRA"+deg+", Decl=@fieldDec"+deg+", Az=@azimuth"+deg+", Alt=@altitude"+deg),
-        ('Parallactic angle', '@paraAngle'+deg),
-        ("Rotator angle", "@rotTelPos"+deg),
-        ("Rotator angle (backup)", "@rotTelPos_backup"+deg),
-        ("Cumulative telescope azimuth", "@cummTelAz"+deg),
+        (
+            "Field coordinates",
+            "RA=@fieldRA"
+            + deg
+            + ", Decl=@fieldDec"
+            + deg
+            + ", Az=@azimuth"
+            + deg
+            + ", Alt=@altitude"
+            + deg,
+        ),
+        ("Parallactic angle", "@paraAngle" + deg),
+        ("Rotator angle", "@rotTelPos" + deg),
+        ("Rotator angle (backup)", "@rotTelPos_backup" + deg),
+        ("Cumulative telescope azimuth", "@cummTelAz" + deg),
         ("Airmass", "@airmass"),
-        ("Moon distance", "@moonDistance"+deg),     
-        ("Moon", "RA=@sunRA"+deg+", Decl=@sunDec"+deg+", Az=@sunAz"+deg+", Alt=@sunAlt"+deg + ", phase=@moonPhase"+deg),
-        ("Sun", "RA=@moonRA"+deg+", Decl=@moonDec"+deg+", Az=@moonAz"+deg+", Alt=@moonAlt"+deg + ", elong=@solarElong"+deg),
+        ("Moon distance", "@moonDistance" + deg),
+        (
+            "Moon",
+            "RA=@sunRA"
+            + deg
+            + ", Decl=@sunDec"
+            + deg
+            + ", Az=@sunAz"
+            + deg
+            + ", Alt=@sunAlt"
+            + deg
+            + ", phase=@moonPhase"
+            + deg,
+        ),
+        (
+            "Sun",
+            "RA=@moonRA"
+            + deg
+            + ", Decl=@moonDec"
+            + deg
+            + ", Az=@moonAz"
+            + deg
+            + ", Alt=@moonAlt"
+            + deg
+            + ", elong=@solarElong"
+            + deg,
+        ),
         ("Sky brightness", "@skyBrightness mag arcsec^-2"),
         ("Exposure time", "@visitExposureTime seconds (@numExposures exposures)"),
         ("Visit time", "@visitTime seconds"),
-        ("Slew distance", "@slewDistance"+deg),
+        ("Slew distance", "@slewDistance" + deg),
         ("Slew time", "@slewTime seconds"),
         ("Field ID", "@fieldId"),
         ("Proposal ID", "@proposalId"),
@@ -57,7 +97,10 @@ def _visits_tooltips(visits, weather=False):
 
     if weather:
         tooltips += [
-            ("Seeing", '@seeingFwhm500" (500nm), @seeingFwhmEff" (Eff), @seeingFwhmGeom" (Geom)'),
+            (
+                "Seeing",
+                '@seeingFwhm500" (500nm), @seeingFwhmEff" (Eff), @seeingFwhmGeom" (Geom)',
+            ),
             ("Cloud", "@cloud"),
             ("5-sigma depth", "@fiveSigmaDepth"),
         ]
@@ -83,6 +126,7 @@ def _add_almanac_events(fig, almanac_events, event_labels, event_colors):
             text_color=event_colors[event],
         )
         fig.add_layout(event_label)
+
 
 def plot_airmass_vs_time(
     visits,
@@ -145,13 +189,13 @@ def plot_airmass_vs_time(
         legend_field="filter",
         name="visit_airmass",
     )
-    
+
     hover_tool = bokeh.models.HoverTool()
     hover_tool.renderers = fig.select({"name": "visit_airmass"})
     hover_tool.tooltips = _visits_tooltips(visits)
-    hover_tool.formatters = {'@start_date': 'datetime'} 
+    hover_tool.formatters = {"@start_date": "datetime"}
     fig.add_tools(hover_tool)
-    
+
     plot_airmass_limit = np.round(np.max(visits.airmass), 1) + 0.1
     fig.y_range = bokeh.models.Range1d(plot_airmass_limit, 1)
     fig.xaxis[0].ticker = bokeh.models.DatetimeTicker()
