bde8938a537cddb1812a3cfd1a0cd4b6d33e1104
Eric Neilsen <neilsen@fnal.gov>
2024-08-06 11:50:53 -0700

    Avoid requiring access to consdb for tests

--- tests/test_consdb.py
+++ tests/test_consdb.py
@@ -8,6 +8,7 @@ from schedview.collect.consdb import read_consdb
 
 class TestConsdb(unittest.TestCase):
 
+    @unittest.skip("avoid requiring access to consdb for tests.")
     def test_consdb_read_consdb(self):
         day_obs: str = "2024-06-26"
         stackers = [
