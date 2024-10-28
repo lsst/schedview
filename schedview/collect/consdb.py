21814be82a2a0ccfaa539d1bf21bd542402a4be6
Eric Neilsen <neilsen@fnal.gov>
2024-10-28 09:24:58 -0700

    better handle empty returns from consdb queries

--- schedview/collect/consdb.py
+++ schedview/collect/consdb.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import rubin_sim.maf
+from rubin_scheduler.scheduler.utils import ObservationArray
 from rubin_scheduler.utils.consdb import load_consdb_visits
 
 
@@ -11,7 +12,12 @@ def read_consdb(
     **kwargs,
 ) -> pd.DataFrame:
     consdb_visits = load_consdb_visits(instrument, *args, **kwargs)
-    visit_records: np.recarray = consdb_visits.opsim.to_records()
+    # If the return is empty, to_records won't be able to determine the
+    # right dtypes, so use ObservationArray()[0:0] instead to make the
+    # empty recarray with the correct dtypes.
+    visit_records: np.recarray = (
+        consdb_visits.opsim.to_records() if len(consdb_visits.opsim) > 0 else ObservationArray()[0:0]
+    )
     for stacker in stackers:
         visit_records = stacker.run(visit_records)
     visits = pd.DataFrame(visit_records)
