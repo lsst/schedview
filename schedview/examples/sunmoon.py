d612ead534dff744f7794432371103c292a76591
Eric Neilsen <neilsen@fnal.gov>
2024-11-21 14:19:51 -0800

    make sunmoon example runnable

--- schedview/examples/sunmoon.py
+++ schedview/examples/sunmoon.py
@@ -1,3 +1,6 @@
+import argparse
+
+import astropy.utils.iers
 import pandas as pd
 from rubin_scheduler.scheduler.model_observatory import ModelObservatory
 
@@ -5,7 +8,9 @@ import schedview.compute.astro
 from schedview import DayObs
 
 
-def sun_moon_positions(day_obs: DayObs = DayObs.from_date("2025-11-11")) -> pd.DataFrame:
+def make_sun_moon_positions(night: str = "2025-11-11") -> pd.DataFrame:
+    day_obs: DayObs = DayObs.from_date(night)
+
     # Get the time for which to get positions: the middle of the night
     # of the provided DayObs.
     night_events: pd.DataFrame = schedview.compute.astro.night_events(day_obs.date)
@@ -14,3 +19,15 @@ def sun_moon_positions(day_obs: DayObs = DayObs.from_date("2025-11-11")) -> pd.D
     observatory: ModelObservatory = ModelObservatory(init_load_length=1, mjd=mjd)
     positions: pd.DataFrame = schedview.compute.astro.compute_sun_moon_positions(observatory)
     return positions
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="sunmoon", description="Write an html file sun and moon coords.")
+    parser.add_argument("night", type=str, help="Evening YYYY-MM-DD")
+    args = parser.parse_args()
+
+    astropy.utils.iers.conf.iers_degraded_accuracy = "ignore"
+
+    positions = make_sun_moon_positions(args.night)
+
+    print(positions)
