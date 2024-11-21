2685126fabcf42cd83f98e9b4d05e5a2207ca261
Eric Neilsen <neilsen@fnal.gov>
2024-11-21 13:20:18 -0800

    example stand alone visit table dashboard

--- schedview/examples/app/visittable.py
+++ schedview/examples/app/visittable.py
@@ -4,8 +4,12 @@ import astropy.utils.iers
 import panel as pn
 import param
 
+from schedview.collect.visits import read_visits
 from schedview.dayobs import DayObs
-from schedview.examples.visittable import visit_table
+from schedview.plot import create_visit_table
+
+STARTUP_VISIBLE_COLUMNS = ["observationStartMJD", "fieldRA", "fieldDec", "filter"]
+TABLE_WIDTH = 1024
 
 
 class VisitTableDashboard(param.Parameterized):
@@ -15,21 +19,38 @@ class VisitTableDashboard(param.Parameterized):
         label="Date",
         doc="Day obs (local calendar date of sunset for the night)",
     )
-
     visit_origin = param.String(
-        default="lsstcomcam", label="Data source", doc="Instrument name or baseline version"
+        default="lsstcomcam", label="Visit source", doc="Instrument or baseline version number"
     )
 
-    @param.depends("night", "visit_origin", watch=True)
+    # Derived parameters
+    day_obs = param.Parameter()
+    visits = param.Parameter()
+
+    @param.depends("night", watch=True)
+    def update_day_obs(self):
+        self.day_obs = DayObs(self.night)
+
+    @param.depends("day_obs", "visit_origin", watch=True)
+    def update_visits(self):
+        self.visits = read_visits(self.day_obs, self.visit_origin)
+
+    @param.depends("visits")
     def visit_table(self):
-        ui_element = visit_table(self.visit_origin, DayObs(self.night))
+        if self.visits is None:
+            return "No visits"
+        ui_element = create_visit_table(
+            visits=self.visits, visible_column_names=STARTUP_VISIBLE_COLUMNS, width=TABLE_WIDTH
+        )
         return ui_element
 
     def make_app(self):
-        self.visit_table()
-
         app = pn.Column(
-            pn.Param(self, parameters=["night", "visit_origin"], widgets={"night": pn.widgets.DatePicker}),
+            pn.Param(
+                self,
+                parameters=["night", "visit_origin"],
+                widgets={"night": pn.widgets.DatePicker},
+            ),
             pn.param.ParamMethod(self.visit_table, loading_indicator=True),
         )
         return app
@@ -42,4 +63,4 @@ if __name__ == "__main__":
         dashboard = VisitTableDashboard()
         return dashboard.make_app()
 
-    pn.serve(make_app, title="Visit Table Dashboard")
+    pn.serve(make_app, title="Tabular Visit Dashboard")
