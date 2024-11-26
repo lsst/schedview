e503e71c1b4ce32967024694877fb93317b57b00
Eric Neilsen <neilsen@fnal.gov>
2024-11-26 11:24:22 -0800

    Inital attempt at riseset for dayobs

--- schedview/dayobs.py
+++ schedview/dayobs.py
@@ -1,15 +1,25 @@
 import copy
 import datetime
 from dataclasses import dataclass
-from functools import cached_property
+from functools import cached_property, partial
 from typing import Literal, Self
 
+import astropy.units as u
 import dateutil.parser
+import numpy as np
+import scipy.optimize
+from astropy.coordinates import AltAz, EarthLocation, HADec, SkyCoord, get_body
 from astropy.time import Time
 
 DAYOBS_TZ = datetime.timezone(datetime.timedelta(hours=-12))
 MJD_EPOCH = datetime.date(1858, 11, 17)
 ONE_DAY = datetime.timedelta(days=1)
+
+LSE30_ATMOSPHERE = {
+    "pressure": 750.0 * 100 * u.Pa,
+    "temperature": 11.5 * u.deg_C,
+    "relative_humidity": 0.4,
+}
 
 IntDateFormat = Literal["mjd", "yyyymmdd", "auto"]
 
@@ -37,6 +47,10 @@ class DayObs:
     # that cannot represent more or less than we want it to.
     date: datetime.date
     int_format: IntDateFormat = "auto"
+    location: EarthLocation = EarthLocation.from_geodetic(
+        lon=-70.7494 * u.deg, lat=-30.2444 * u.deg, height=2650.0 * u.meter
+    )
+    atmosphere: dict | None = None
 
     @classmethod
     def from_date(cls, arg: datetime.date | int | str | Self, int_format: IntDateFormat = "auto") -> Self:
@@ -80,7 +94,7 @@ class DayObs:
                 return copy.deepcopy(arg)
             case datetime.date():
                 date = arg
-            case int():
+            case int() | np.int64():
                 if int_format == "yyyymmdd" or (
                     len(str(arg)) == len(str("YYYYMMDD")) and int_format == "auto"
                 ):
@@ -89,8 +103,9 @@ class DayObs:
                 else:
                     if int_format not in ("auto", "mjd"):
                         raise ValueError("Invalid integer format.")
-
-                    date = MJD_EPOCH + datetime.timedelta(days=arg)
+                    # We need the cast because a np.int64 confuses the
+                    # datetime.datetime constructor.
+                    date = MJD_EPOCH + datetime.timedelta(days=int(arg))
             case "yesterday":
                 date = (datetime.datetime.now(tz=DAYOBS_TZ) - ONE_DAY).date()
             case "today" | "tonight":
@@ -187,6 +202,106 @@ class DayObs:
             datetime.datetime(self.date.year, self.date.month, self.date.day, tzinfo=DAYOBS_TZ) + ONE_DAY
         )
         return Time(end_datetime)
+
+    @cached_property
+    def mean_local_solar_midnight(self):
+        mjd = self.start.mjd + (-1 * self.location.lon.deg / 360 - self.start.mjd) % 1
+        assert self.start.mjd <= mjd <= self.end.mjd
+        return Time(mjd, format="mjd")
+
+    @cached_property
+    def mean_local_solar_noon(self):
+        mjd = self.start.mjd + (self.mean_solar_midnight.mjd + 0.5 - self.start.mjd) % 1
+        assert self.start.mjd <= mjd <= self.end.mjd
+        return Time(mjd, format="mjd")
+
+    def rough_body_coordinates(self, body):
+        return get_body(body, time=self.mean_local_solar_midnight, location=self.location)
+
+    def _coords_time_in_vaccuum(
+        self,
+        coords: SkyCoord,
+        alt: float = 0.0,
+        direction: Literal["rise", "set"] = "set",
+    ) -> Time:
+        ha_at_mean_solar_midnight = coords.transform_to(
+            HADec(obstime=self.mean_local_solar_midnight, location=self.location)
+        ).ha.deg
+        ha_sign = -1 if direction == "rise" else 1
+        cos_event_ha = (np.sin(np.radians(alt)) - np.sin(coords.dec.rad) * np.sin(self.location.lat.rad)) / (
+            np.cos(coords.dec.rad) * np.cos(self.location.lat.rad)
+        )
+        if abs(cos_event_ha) > 1:
+            raise ValueError(f"Coordinates never reach {alt} on {self}.")
+        event_hour_angle = ha_sign * np.degrees(np.arccos(cos_event_ha))
+        ha_diff = (event_hour_angle - ha_at_mean_solar_midnight + 180) % 360 - 180
+        mjd = self.mean_local_solar_midnight.mjd + ha_diff * (0.9972696 / 360)
+        mjd = self.start.mjd + (mjd - self.start.mjd) % 1
+        time = Time(mjd, format="mjd")
+        return time
+
+    def body_time(
+        self,
+        body: Literal["sun", "moon"] = "sun",
+        alt: float = 0.0,
+        direction: Literal["rise", "set"] = "set",
+    ) -> Time:
+
+        if self.atmosphere is None:
+            get_altaz = partial(AltAz, location=self.location)
+        else:
+            get_altaz = partial(AltAz, location=self.location, **self.atmosphere)
+
+        # Get the coordinates of the body at midnight, and use direct
+        # spherical trig to find when that reaches the desired alt in vaccuum.
+        # This will then be used as the initial guess by the solver.
+        rough_coords = self.rough_body_coordinates(body)
+        rough_time = self._coords_time_in_vaccuum(rough_coords, alt, direction)
+
+        # We'll use this to get the optimizer to go to the right minimum
+        opposite_direction = "set" if direction == "rise" else "rise"
+        opposite_time = self._coords_time_in_vaccuum(rough_coords, alt, opposite_direction)
+
+        def abs_delta_alt(mjd):
+            obstime = Time(mjd, format="mjd")
+            sun = get_body(body, obstime)
+            altaz = get_altaz(obstime=obstime)
+            alt_at_mjd = sun.transform_to(altaz).alt.deg
+            return np.abs(alt_at_mjd - alt)
+
+        # Set the bounds so that the optimized minimum found is the one
+        # that is closest to the rough minimum, thereby preventing it
+        # from jumping to the wrong minumum (providing the setting time
+        # when the rising time was requested, or vice versa).
+        midpoint_mjd = (rough_time.mjd + opposite_time.mjd) / 2
+        if opposite_time < rough_time:
+            opt_bounds = [(max(self.start.mjd, midpoint_mjd), self.end.mjd)]
+        else:
+            opt_bounds = [(self.start.mjd, min(midpoint_mjd, self.end.mjd))]
+
+        solution = scipy.optimize.minimize(abs_delta_alt, rough_time.mjd, bounds=opt_bounds, method="Powell")
+        assert solution.success, f"Minimizer failed: {solution.message}"
+        optimized_mjd = solution.x[0]
+
+        # When an event does not happen at all in a day_obs (which can happen
+        # for the moon, or for the sun in the arctic circle), the optimizer
+        # finds a value near the bounds, and the alt at the optimized time is
+        # not what we requested. Raise an exception when this happens.
+        if abs_delta_alt(optimized_mjd) > 0.01:
+            assert (optimized_mjd - opt_bounds[0][0] < 0.01) or (opt_bounds[0][1] - optimized_mjd < 0.01)
+            raise ValueError(f"The body {body} never reaches {alt} during {direction}")
+
+        optimized_time = Time(optimized_mjd, format="mjd")
+
+        return optimized_time
+
+    @cached_property
+    def sunset(self):
+        return self.body_time("sun", alt=0.0, direction="set")
+
+    @cached_property
+    def sunrise(self):
+        return self.body_time("sun", alt=0.0, direction="rise")
 
     def __int__(self) -> int:
         return self.mjd if self.int_format in ("auto", "mjd") else self.yyyymmdd
--- tests/test_dayobs.py
+++ tests/test_dayobs.py
@@ -2,9 +2,21 @@ import datetime
 import unittest
 from collections import namedtuple
 
+import astropy.units as u
+import numpy as np
+import pandas as pd
+from astropy.coordinates import AltAz, get_body
 from astropy.time import Time
 
 from schedview import DayObs
+
+try:
+    from rubin_scheduler.site_models.almanac import Almanac
+
+    ALMANAC = Almanac()
+except ModuleNotFoundError:
+    ALMANAC = None
+
 
 DayObsTestData = namedtuple("DayObsTestData", ("date", "yyyymmdd", "iso_date", "mjd", "iso_times"))
 
@@ -54,3 +66,86 @@ class TestDayObs(unittest.TestCase):
                 self.assert_equal(DayObs.from_time(t.datetime), d)
                 self.assert_equal(DayObs.from_time(t.iso), d)
                 self.assert_equal(DayObs.from_time(t.mjd), d)
+
+    def test_rs_time(self):
+        num_nights_tested = 10
+
+        rng = np.random.default_rng(seed=6563)
+        test_mjds = rng.choice(
+            np.arange(DayObs.from_date("2024-10-01").mjd, DayObs.from_date("2036-10-01").mjd),
+            num_nights_tested,
+            replace=False,
+        )
+        test_directions = ["rise", "set"]
+        test_alts = [-18, -12, -6, 0]
+        test_bodies = ["sun", "moon"]
+
+        for mjd in test_mjds:
+            day_obs = DayObs.from_date(mjd, int_format="mjd")
+            for body in test_bodies:
+                for direction in test_directions:
+                    for alt in test_alts:
+                        try:
+                            event_time = day_obs.body_time(body, alt, direction)
+                        except ValueError:
+                            # Maybe the event just never happens on night.
+                            # But, the sun has the tested events every night.
+                            assert body != "sun"
+                            continue
+
+                        event_altaz = AltAz(obstime=event_time, location=day_obs.location)
+                        event_alt = get_body(body, event_time).transform_to(event_altaz).alt.deg
+                        assert np.isclose(alt, event_alt, atol=0.01)
+
+                        assert DayObs.from_time(event_time).mjd == day_obs.mjd
+
+                        after_event_time = event_time + 1 * u.minute
+                        after_event_altaz = AltAz(obstime=after_event_time, location=day_obs.location)
+                        after_event_alt = (
+                            get_body(body, after_event_time).transform_to(after_event_altaz).alt.deg
+                        )
+
+                        before_event_time = event_time - 1 * u.minute
+                        before_event_altaz = AltAz(obstime=before_event_time, location=day_obs.location)
+                        before_event_alt = (
+                            get_body(body, before_event_time).transform_to(before_event_altaz).alt.deg
+                        )
+
+                        if direction == "rise":
+                            assert before_event_alt <= alt <= after_event_alt
+                        else:
+                            assert before_event_alt >= alt >= after_event_alt
+
+    @unittest.skipUnless(ALMANAC is not None, "rubin_scheduler almanac not available.")
+    def test_against_almanac(self):
+        num_nights_tested = 10
+        almanac_times = (
+            pd.DataFrame(ALMANAC.sunsets)
+            .sample(num_nights_tested, random_state=42)
+            .set_index("sunset", drop=False)
+        )
+        for sunset_mjd, night_times in almanac_times.iterrows():
+            day_obs = DayObs.from_time(sunset_mjd)
+            assert np.isclose(night_times.sunset, day_obs.body_time("sun", 0, "set").mjd)
+            assert np.isclose(night_times.sunrise, day_obs.body_time("sun", 0, "rise").mjd)
+            assert np.isclose(night_times.sun_n12_setting, day_obs.body_time("sun", -12, "set").mjd)
+            assert np.isclose(night_times.sun_n12_rising, day_obs.body_time("sun", -12, "rise").mjd)
+            assert np.isclose(night_times.sun_n18_setting, day_obs.body_time("sun", -18, "set").mjd)
+            assert np.isclose(night_times.sun_n18_rising, day_obs.body_time("sun", -18, "rise").mjd)
+            try:
+                assert (
+                    np.isclose(night_times.moonset, day_obs.body_time("moon", 0, "set").mjd)
+                    or DayObs.from_time(night_times.moonset).mjd != day_obs.mjd
+                )
+            except ValueError:
+                # There might not be a moonset on this day_obs
+                assert DayObs.from_time(night_times.moonset).mjd != day_obs.mjd
+
+            try:
+                assert (
+                    np.isclose(night_times.moonrise, day_obs.body_time("moon", 0, "rise").mjd)
+                    or DayObs.from_time(night_times.moonrise).mjd != day_obs.mjd
+                )
+            except ValueError:
+                # There might not be a moonrise on this day_obs
+                assert DayObs.from_time(night_times.moonrise).mjd != day_obs.mjd
