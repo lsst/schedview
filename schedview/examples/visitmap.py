a51ebd6658cff317feec2902177fcc8ce6cbce70
Eric Neilsen <neilsen@fnal.gov>
2025-01-17 12:15:31 -0800

    sample visitmap app

--- schedview/examples/visitmap.py
+++ schedview/examples/visitmap.py
@@ -1,10 +1,11 @@
 import argparse
 
 import astropy.utils.iers
+import bokeh.embed
 import bokeh.io
+import bokeh.models
 import pandas as pd
 import uranography.api
-from bokeh.models.ui.ui_element import UIElement
 from rubin_scheduler.scheduler.model_observatory.model_observatory import ModelObservatory
 
 import schedview.collect.visits
@@ -14,48 +15,73 @@ from schedview.dayobs import DayObs
 
 
 def make_visit_map(
-    night: str = "2026-03-15",
-    visit_source: str = "3.5",
+    iso_date: str,
+    visit_source: str,
     nside: int = 8,
-    map_classes=[uranography.api.ArmillarySphere],
-) -> UIElement:
+    map_classes=[uranography.api.ArmillarySphere, uranography.api.Planisphere],
+    report: None | str = None,
+) -> bokeh.models.UIElement:
+    """_summary_
 
-    day_obs: DayObs = DayObs.from_date(night)
+    Parameters
+    ----------
+    iso_date : `str`
+        Local calendar date of the evening on which the night starts,
+        in YYYY-MM-DD (ISO 8601) format.
+    visit_source : `str`
+        Instrument or baseline version number.
+    nside: `int`, optional
+        The nside of the map to show, by default 8.
+    map_classes: `list`, optional
+        A list of uranography map classes to use, by default
+        `[uranography.api.ArmillarySphere, uranography.api.Planisphere]`.
+    report : `None` | `str`, optional
+        Report file name, by default ``None`` (to not write to a file).
 
-    # Collect the data to be shown
+    Returns
+    -------
+    result : `bokeh.models.UIElement`
+        The bokeh plot object with the map(s) of visits.
+    """
+
+    # Parameters
+    day_obs: DayObs = DayObs.from_date(iso_date)
+
+    # Collect
     visits: pd.DataFrame = schedview.collect.visits.read_visits(
         day_obs, visit_source, stackers=schedview.collect.visits.NIGHT_STACKERS
     )
-
     footprint = schedview.collect.footprint.get_footprint(nside)
+
+    # Compute
     observatory: ModelObservatory = ModelObservatory(nside=nside, init_load_length=1)
     observatory.mjd = visits.observationStartMJD.max()
     conditions = observatory.return_conditions()
-
-    # Do computations required by mapping code
     visits: pd.DataFrame = schedview.compute.visits.add_coords_tuple(visits)
 
-    # Make the map
-    sky_map: UIElement = schedview.plot.visitmap.plot_visit_skymaps(
+    # Plot
+    result: bokeh.models.UIElement = schedview.plot.visitmap.plot_visit_skymaps(
         visits, footprint, conditions, map_classes=map_classes
     )
-    return sky_map
+
+    # Report
+    if report is not None:
+        with open(report, "w") as report_io:
+            print(bokeh.embed.file_html(result), file=report_io)
+
+    return result
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog="visittable", description="Write an html file with map of visits.")
-    parser.add_argument("filename", type=str, help="output file name")
-    parser.add_argument("night", type=str, help="Evening YYYY-MM-DD")
+    parser = argparse.ArgumentParser(prog="visitmap", description="Make interactive maps of visits.")
+    parser.add_argument("date", type=str, help="Evening YYYY-MM-DD")
     parser.add_argument(
         "visit_source", type=str, default="lsstcomcam", help="Instrument or baseline version number"
     )
+    parser.add_argument("report", type=str, help="output file name")
     parser.add_argument("--nside", type=int, default=32, help="nside of map to show")
     args = parser.parse_args()
 
     astropy.utils.iers.conf.iers_degraded_accuracy = "ignore"
 
-    figure = make_visit_map(args.night, args.visit_source, nside=args.nside)
-
-    # You can also save html fragments suitable for embedding in other pages.
-    # See https://docs.bokeh.org/en/latest/docs/user_guide/output/embed.html
-    bokeh.io.save(figure, args.filename)
+    make_visit_map(args.date, args.visit_source, args.nside, report=args.report)
--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,14 +1,20 @@
+import os
 import unittest
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
 import astropy.utils.exceptions
 import astropy.utils.iers
+from rubin_scheduler.utils import SURVEY_START_MJD
 
+from schedview.dayobs import DayObs
 from schedview.examples.gaps import make_gaps
 from schedview.examples.nightevents import make_night_events
+from schedview.examples.visitmap import make_visit_map
 
 astropy.utils.iers.conf.iers_degraded_accuracy = "ignore"
+
+TEST_ISO_DATE = str(DayObs.from_date(int(SURVEY_START_MJD), int_format="mjd"))
 
 
 class TestExamples(unittest.TestCase):
@@ -16,7 +22,7 @@ class TestExamples(unittest.TestCase):
     def test_nightevents(self):
         with TemporaryDirectory() as dir:
             report = Path(dir).joinpath("nightevents.txt").name
-            make_night_events("2026-03-15", report)
+            make_night_events(TEST_ISO_DATE, report)
             with open(report) as report_io:
                 content = report_io.read()
                 assert len(content.split("\n")) == 12
@@ -24,8 +30,14 @@ class TestExamples(unittest.TestCase):
     def test_gaps(self):
         with TemporaryDirectory() as dir:
             report = Path(dir).joinpath("gaps.html").name
-            make_gaps("2026-03-15", "baseline", report)
+            make_gaps(TEST_ISO_DATE, "baseline", report)
             with open(report) as report_io:
                 content = report_io.read()
                 assert "Open shutter" in content
                 assert "Mean gap time" in content
+
+    def test_visitmap(self):
+        with TemporaryDirectory() as dir:
+            report = Path(dir).joinpath("visitmaps.html").name
+            make_visit_map(TEST_ISO_DATE, "baseline", 16, report=report)
+            assert os.path.exists(report)
