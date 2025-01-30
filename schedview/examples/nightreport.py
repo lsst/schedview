dc3df9f250f1d46114629f439f9e448fd23ee001
Eric Neilsen <neilsen@fnal.gov>
2025-01-30 08:52:19 -0800

    match new nightreport API in standalone example

--- schedview/examples/nightreport.py
+++ schedview/examples/nightreport.py
@@ -12,13 +12,12 @@ from schedview.compute.nightreport import best_night_report
 def format_night_report(
     night: str,
     telescope: Literal["AuxTel", "Simonyi"],
-    api_endpoint: str = "https://usdf-rsp-dev.slac.stanford.edu/nightreport/reports",
     user_params: dict | None = None,
 ) -> str:
     day_obs: DayObs = DayObs.from_date(night)
 
     # Query consdb for all night reports for the night
-    night_reports: list[dict] = get_night_report(day_obs, telescope, api_endpoint, user_params)
+    night_reports: list[dict] = get_night_report(day_obs, telescope, user_params)
 
     # Try to guess the best one
     best_version_of_night_report: dict = best_night_report(night_reports)
@@ -34,15 +33,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="nightreport", description="Print the night report as markdown.")
     parser.add_argument("night", type=str, help="Evening YYYY-MM-DD")
     parser.add_argument("--telescope", type=str, default="Simonyi", help="Evening YYYY-MM-DD")
-    parser.add_argument(
-        "--api-endpoint",
-        type=str,
-        default="https://usdf-rsp-dev.slac.stanford.edu/nightreport/reports",
-        help="Night report API endpoint URI",
-    )
     args = parser.parse_args()
 
     astropy.utils.iers.conf.iers_degraded_accuracy = "ignore"
 
-    night_report_md: str = format_night_report(args.night, args.visit_source, nside=args.nside)
+    night_report_md: str = format_night_report(args.night, args.telescope)
     print(night_report_md)
