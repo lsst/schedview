93047c1f23f7a947f881aa8114c3374575e2a1e8
Eric Neilsen <neilsen@fnal.gov>
2025-01-30 09:32:32 -0800

    sunmoon example

--- schedview/examples/sunmoon.py
+++ schedview/examples/sunmoon.py
@@ -1,33 +1,73 @@
 import argparse
 
-import astropy.utils.iers
 import pandas as pd
 from rubin_scheduler.scheduler.model_observatory import ModelObservatory
 
-import schedview.compute.astro
-from schedview import DayObs
+import schedview.compute.visits
+import schedview.plot
+from schedview.dayobs import DayObs
 
 
-def make_sun_moon_positions(night: str = "2025-11-11") -> pd.DataFrame:
-    day_obs: DayObs = DayObs.from_date(night)
+def make_sunmoon(
+    iso_date: str,
+    report: None | str = None,
+) -> str:
+    """Make sun and moon tables.
 
-    # Get the time for which to get positions: the middle of the night
-    # of the provided DayObs.
+    Parameters
+    ----------
+    iso_date : `str`
+        Local calendar date of the evening on which the night starts,
+        in YYYY-MM-DD (ISO 8601) format.
+    report : `None` | `str`, optional
+        Report file name, by default ``None`` (to not write to a file).
+
+    Returns
+    -------
+    bokeh.models.UIElement
+        _description_
+    """
+
+    # Parameters
+    day_obs: DayObs = DayObs.from_date(iso_date)
+
+    # Collect
+
+    # Compute
     night_events: pd.DataFrame = schedview.compute.astro.night_events(day_obs.date)
     mjd = night_events.loc["night_middle", "MJD"]
 
     observatory: ModelObservatory = ModelObservatory(init_load_length=1, mjd=mjd)
     positions: pd.DataFrame = schedview.compute.astro.compute_sun_moon_positions(observatory)
-    return positions
+
+    # Plot
+    result: str = f"""## Sun and moon
+
+### Events
+
+{night_events.to_markdown()}
+
+Modified Julian Date (MJD) is in units of days (UTC). Local Sidereal Time (LST) is in units of degrees.
+
+### Positions at local solar midnight
+
+{positions.to_markdown()}
+"""
+
+    # Report
+    if report is not None:
+        with open(report, "w") as report_io:
+            print(result, file=report_io)
+
+    return result
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog="sunmoon", description="Write an html file sun and moon coords.")
-    parser.add_argument("night", type=str, help="Evening YYYY-MM-DD")
+    parser = argparse.ArgumentParser(
+        prog="sunmoon", description="Make a table of sun and moon events and positions."
+    )
+    parser.add_argument("date", type=str, help="Evening YYYY-MM-DD")
+    parser.add_argument("report", type=str, help="output file name")
     args = parser.parse_args()
 
-    astropy.utils.iers.conf.iers_degraded_accuracy = "ignore"
-
-    positions = make_sun_moon_positions(args.night)
-
-    print(positions)
+    make_sunmoon(args.date, args.report)
