2a6172d8f0aa52440ec564d1752aaa148678c6b9
Eric Neilsen <neilsen@fnal.gov>
2025-01-31 09:04:57 -0800

    overhead table example

--- schedview/examples/overheadtable.py
+++ schedview/examples/overheadtable.py
@@ -3,53 +3,75 @@ import argparse
 import astropy.utils.iers
 import pandas as pd
 
-import schedview.collect.visits
-import schedview.compute.astro
-import schedview.compute.visits
-import schedview.plot
-from schedview import DayObs
+from schedview.collect.visits import NIGHT_STACKERS, read_visits
+from schedview.dayobs import DayObs
 
 
 def make_overhead_table(
-    night: str = "2026-11-11",
-    visit_source: str = "3.5",
+    iso_date: str, visit_source: str, report: None | str = None, min_time: float = 30
 ) -> str:
+    """Create a table of overheads between visits.
 
-    day_obs: DayObs = DayObs.from_date(night)
+    Parameters
+    ----------
+    iso_date : `str`
+        Local calendar date of the evening on which the night starts,
+        in YYYY-MM-DD (ISO 8601) format.
+    visit_source : `str`
+        Instrument or baseline version number.
+    report : `None` | `str`, optional
+        Report file name, by default ``None`` (to not write to a file).
+    min_time : `float`, optional
+        The minimim duration of gaps to show (seconds). Defaults to 30.
 
-    # Get the time for which to get positions: the middle of the night
-    # of the provided DayObs.
-    night_events: pd.DataFrame = schedview.compute.astro.night_events(day_obs.date)
+    Returns
+    -------
+    result : `str`
+        The table of long overheads.
+    """
 
-    # Get the visits
-    visits: pd.DataFrame = schedview.collect.visits.read_visits(
-        day_obs, visit_source, stackers=schedview.collect.visits.NIGHT_STACKERS
+    # Parameters
+    day_obs: DayObs = DayObs.from_date(iso_date)
+
+    # Collect
+    visits: pd.DataFrame = read_visits(day_obs, visit_source, stackers=NIGHT_STACKERS)
+
+    # Compute
+    for time_column in "obs_start", "observationStartDatetime64":
+        if time_column in visits:
+            break
+
+    visits["previous_filter"] = visits["filter"].shift(1)
+    long_gap_visits: pd.DataFrame = (
+        visits.sort_values("overhead", ascending=False)
+        .query(f"overhead>{min_time}")
+        .loc[:, [time_column, "overhead", "slewDistance", "filter", "previous_filter"]]
+        .sort_values(time_column)
     )
 
-    overhead_summary = schedview.compute.visits.compute_overhead_summary(
-        visits,
-        night_events.loc["sun_n12_setting", "MJD"],
-        night_events.loc["sun_n12_rising", "MJD"],
-    )
-    summary_table = schedview.plot.create_overhead_summary_table(overhead_summary)
-    return summary_table
+    # Plot
+    result: str = long_gap_visits.to_markdown()
+
+    # Report
+    if report is not None:
+        with open(report, "w") as report_io:
+            print(result, file=report_io)
+
+    return result
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        prog="overheadtable", description="Write an html file visit overhead data."
+        prog="overheadtable", description="Create a table of long overheads between visits for a night."
     )
-    parser.add_argument("filename", type=str, help="output file name")
-    parser.add_argument("night", type=str, help="Evening YYYY-MM-DD")
+    parser.add_argument("date", type=str, help="Evening YYYY-MM-DD")
     parser.add_argument(
         "visit_source", type=str, default="lsstcomcam", help="Instrument or baseline version number"
     )
+    parser.add_argument("report", type=str, help="output file name")
+    parser.add_argument("--dt", type=float, default=30.0, help="Minimum gap duration to show (seconds)")
     args = parser.parse_args()
 
     astropy.utils.iers.conf.iers_degraded_accuracy = "ignore"
 
-    overhead = make_overhead_table(args.night, args.visit_source)
-    with open(args.filename, "w") as html_io:
-        print("<html><body>", file=html_io)
-        print(overhead, file=html_io)
-        print("</body></html>", file=html_io)
+    make_overhead_table(args.date, args.visit_source, args.report, args.dt)
