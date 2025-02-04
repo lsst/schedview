688bc84bedc3b7abe802ff743e75cc32cebf884a
Eric Neilsen <neilsen@fnal.gov>
2025-02-04 09:18:09 -0800

    use token arg over env

--- schedview/collect/auth.py
+++ schedview/collect/auth.py
@@ -12,12 +12,15 @@ except ImportError:
 
     @cache
     def get_auth(token_file: str | None = None) -> tuple[str, str]:
-        token = os.environ.get("ACCESS_TOKEN")
-        if token is None and token_file is not None:
+        token: str | None = None
+
+        if token_file is not None:
             with open("token_file", "r") as f:
                 token = f.read()
+        elif "ACCESS_TOKEN" in os.environ:
+            token = os.environ.get("ACCESS_TOKEN")
 
-        if not isinstance(token, str):
+        if token is None:
             raise ValueError("No access token found.")
 
         return ("user", token)
