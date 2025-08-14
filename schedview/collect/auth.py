9f5566bf43cb5c6b23f9149aa0cb0cdbf22ebdf7
Eric Neilsen <neilsen@fnal.gov>
2025-08-14 14:14:43 -0700

    Read token from file if ACCESS_TOKEN_FILE set

--- schedview/collect/auth.py
+++ schedview/collect/auth.py
@@ -1,11 +1,16 @@
 import os
 from functools import cache
 
+ACCESS_TOKEN_FILE = os.environ.get("ACCESS_TOKEN_FILE", None)
+
 try:
     import lsst.rsp
 
     @cache
     def get_auth(*args, **kwargs) -> tuple[str, str]:
+        if len(args) == 0 and "tokenfile" not in kwargs:
+            kwargs["tokenfile"] = ACCESS_TOKEN_FILE
+
         return ("user", lsst.rsp.get_access_token(*args, **kwargs))
 
 except ImportError:
@@ -13,6 +18,9 @@ except ImportError:
     @cache
     def get_auth(token_file: str | None = None) -> tuple[str, str]:
         token: str | None = None
+
+        if token_file is None:
+            token_file = ACCESS_TOKEN_FILE
 
         if token_file is not None:
             with open("token_file", "r") as f:
