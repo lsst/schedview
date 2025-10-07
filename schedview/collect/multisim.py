b3af1489dd7c0ecd0ce327226fe7218acf324d41
Eric Neilsen <neilsen@fnal.gov>
2025-10-07 08:59:52 -0700

    start of move to new sim arcihve

--- schedview/collect/multisim.py
+++ schedview/collect/multisim.py
@@ -1,22 +1,21 @@
+import datetime
 from typing import Literal
 
-import astropy
-import astropy.time
 import numpy as np
 import pandas as pd
-import rubin_sim
-from lsst.resources import ResourcePath
 from rubin_scheduler.scheduler.utils import ObservationArray, SchemaConverter
+from rubin_sim.sim_archive import vseqarchive
+from rubin_sim.sim_archive.vseqmetadata import VisitSequenceArchiveMetadata
 
-from ..collect.visits import NIGHT_STACKERS
-from ..compute.visits import add_coords_tuple
-from .opsim import all_visits_columns, read_opsim
+from schedview import DayObs
+from schedview.collect.visits import NIGHT_STACKERS
+from schedview.compute.visits import add_coords_tuple
 
 
-def read_multiple_opsims(
-    archive_uri: str,
-    sim_date: str,
-    day_obs_mjd: int,
+def read_multiple_prenights(
+    vsarch: VisitSequenceArchiveMetadata,
+    sim_date: datetime.date | int | str | DayObs,
+    day_obs: datetime.date | int | str | DayObs,
     stackers: list | None = NIGHT_STACKERS,
     telescope: Literal["AuxTel", "Simonyi", "auxtel", "simonyi", "maintel"] = "simonyi",
 ):
@@ -24,12 +23,12 @@ def read_multiple_opsims(
 
     Parameters
     ----------
-    archive_uri : `str`
-        The URI of the sim archive.
-    sim_date : `str`
-        The date on which the simulations to be run.
-    day_obs_mjd : `int`
-        The day_obs MJD of the first night for which to load visits.
+    vsarch : `VisitSequencArchiveMetadata`
+        The interface to the visit sequence archive metadata.
+    sim_date : `datetime.date` or `int` or `str` or `DayObs`
+        The date (dayobs) on which the simulations to be run.
+    day_obs_mjd : `datetime.date` or `int` or `str` or `DayObs`
+        The day_obs of the first night for which to load visits.
     stackers : `list` or `None`
         A list of stackers to apply.
     telescope : `str`
@@ -40,59 +39,41 @@ def read_multiple_opsims(
     visits : `pandas.DataFrame`
         Data on the visits.
     """
+    sim_date = DayObs.from_date(sim_date)
+    assert isinstance(sim_date, DayObs)
+
+    day_obs = DayObs.from_date(day_obs)
+    assert isinstance(day_obs, DayObs)
 
     telescope = "simonyi" if telescope.lower() in ("simonyi", "maintel") else "auxtel"
     assert telescope in ("simonyi", "auxtel")
 
-    sims_metadata = rubin_sim.sim_archive.read_archived_sim_metadata(
-        archive_uri, latest=sim_date, num_nights=1
-    )
+    max_age = (datetime.date.today() - sim_date.date).days + 1
+    prenights_for_night = vsarch.sims_on_nights(
+        day_obs.date, day_obs.date, tags=["prenight"], telescope=telescope, max_simulation_age=max_age
+    ).set_index(["visitseq_uuid"])
 
     visits_list = []
-    for sim_uri, sim_metadata in sims_metadata.items():
-        if "telescope" not in sim_metadata or sim_metadata["telescope"] != telescope:
-            # This sim is for the wrong telescope, so skip it.
-            continue
-
-        version_key = (
-            "opsim_config_version" if "opsim_config_version" in sim_metadata else "opsim_config_branch"
+    for visitseq_uuid, prenight_metadata in prenights_for_night.iterrows():
+        these_visits = vseqarchive.get_visits(
+            prenight_metadata["visitseq_url"],
+            query=f"floor(observationStartMJD-0.5)=={day_obs.mjd}",
+            stackers=stackers,
         )
+        these_visits["visitseq_uuid"] = visitseq_uuid
+        these_visits = add_coords_tuple(these_visits)
+
         sim_metadata_keys = [
-            "label",
-            version_key,
-            "opsim_config_repository",
-            "opsim_config_script",
-            "scheduler_version",
+            "visitseq_label",
+            "config_url",
+            "schedulre_version",
             "sim_runner_kwargs",
+            "sim_creation_day_obs",
+            "daily_id",
             "tags",
         ]
-
-        first_day_obs_mjd = astropy.time.Time(sim_metadata["simulated_dates"]["first"]).mjd
-        last_day_obs_mjd = astropy.time.Time(sim_metadata["simulated_dates"]["last"]).mjd
-
-        # Make mypy happy
-        assert isinstance(first_day_obs_mjd, float)
-        assert isinstance(last_day_obs_mjd, float)
-
-        includes_day_obs = first_day_obs_mjd <= day_obs_mjd <= last_day_obs_mjd
-
-        if not includes_day_obs:
-            continue
-
-        sim_rp = ResourcePath(sim_uri).join(sim_metadata["files"]["observations"]["name"])
-
-        these_visits = read_opsim(
-            sim_rp,
-            constraint=f"FLOOR(observationStartMJD-0.5)={day_obs_mjd}",
-            stackers=stackers,
-            dbcols=all_visits_columns(),
-        )
-        these_visits = add_coords_tuple(these_visits)
-        these_visits["sim_date"] = sim_uri.split("/")[-3]
-        these_visits["sim_index"] = int(sim_uri.split("/")[-2])
-
         for key in sim_metadata_keys:
-            value = sim_metadata[key] if key in sim_metadata else None
+            value = prenight_metadata[key] if key in prenight_metadata else None
             these_visits[key] = [value] * len(these_visits)
 
         visits_list.append(these_visits)
@@ -103,17 +84,11 @@ def read_multiple_opsims(
         # Make a DataFrame with the expected columns and no rows.
         visits = SchemaConverter().obs2opsim(ObservationArray()[0:0])
         visits["start_timestamp"] = pd.Series(dtype=np.dtype("<M8[ns]"))
-        visits["sim_index"] = pd.Series(dtype=np.dtype("int64"))
-        expected_sim_metadata_keys = [
-            "sim_date",
-            "label",
-            "opsim_config_version",
-            "opsim_config_repository",
-            "opsim_config_script",
-            "scheduler_version",
-            "sim_runner_kwargs",
-            "tags",
-        ]
-        for key in expected_sim_metadata_keys:
+        visits["daily_id"] = pd.Series(dtype=np.dtype("int64"))
+        for key in sim_metadata_keys:
+            if key in visits.columns:
+                continue
             visits[key] = pd.Series()
+
+    visits.rename(columns={"visitseq_label": "label", "daily_id": "sim_index"}, inplace=True)
     return visits
