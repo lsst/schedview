44bf8ddc2f5ceeebe34fd40a4c588cc7c1233ada
Eric Neilsen <neilsen@fnal.gov>
2025-10-15 09:41:07 -0700

    update prenight_inventory for new vseq database

--- schedview/app/prenight_inventory.py
+++ schedview/app/prenight_inventory.py
@@ -1,29 +1,27 @@
 import argparse
 import datetime
-import os
 
 import pandas as pd
-from rubin_sim.sim_archive import read_archived_sim_metadata
+from rubin_sim.sim_archive.prenightindex import get_prenight_index
 
-from schedview.compute import munge_sim_archive_metadata
 from schedview.dayobs import DayObs
 
-SIM_TABLE_COLUMNS = (
-    "resource_url",
-    "sim_execution_date",
-    "sim_index",
+TELESCOPES = ("simonyi", "auxtel")
+
+SIM_TABLE_COLUMNS = [
+    "visitseq_uuid",
+    "sim_creation_day_obs",
+    "daily_id",
     "telescope",
     "scheduler_version",
-    "opsim_config_version",
-    "opsim_config_script",
+    "config_url",
+    "visitseq_url",
     "tags",
-)
+]
 
 
 def get_prenight_table(
     day_obs: DayObs,
-    archive_uri="s3://rubin:rubin-scheduler-prenight/opsim/",
-    num_nights=7,
 ) -> str:
     """Create a data frame of pre-night simulations for a day obs.
 
@@ -31,48 +29,23 @@ def get_prenight_table(
     ----------
     day_obs : `schedview.DayObs`
         The date for which to get pre-night simulations.
-    archive_uri : `str`
-        The URL for the archive in which to look for simulations.
-    num_nights : `int`
-        Then number of nights before the requested night for which to look
-        for simulatiotns.
 
     Returns
     -------
-    result : `pandas.DataFrame`
+    result : `str`
         The table of prenight simulations.
     """
 
-    # Make sure conditions are met.
-    if archive_uri.startswith("s3://rubin:rubin-scheduler-prenight"):
-        # If the archive is the standard USDF one, fail with an informative
-        # error if the environment is not set up to do that.
-        # Otherwise, it will fail with an uninformative error.
-        if os.environ["LSST_DISABLE_BUCKET_VALIDATION"] != "1":
-            raise RuntimeError("Environment variable LSST_DISABLE_BUCKET_VALIDATION must be set to 1")
-
-    if archive_uri.startswith("s3:"):
-        if "S3_ENDPOINT_URL" not in os.environ:
-            raise RuntimeError(
-                "Environment variable  S3_ENDPOINT_URL must be set, "
-                + "perhaps to https://s3dfrgw.slac.stanford.edu/ ."
-            )
-
     # Collect
-    raw_sim_metadata = read_archived_sim_metadata(
-        archive_uri, num_nights=num_nights, compilation_resource=f"{archive_uri}compiled_metadata_cache.h5"
-    )
+    raw_sim_metadata = [get_prenight_index(day_obs.date, telescope=t) for t in TELESCOPES]
 
     # Compute
-    sim_metadata = munge_sim_archive_metadata(raw_sim_metadata, day_obs, archive_uri)
-    prenight_df = pd.DataFrame(sim_metadata).T
-    prenight_df.index.name = "resource_url"
+    sim_metadata = pd.concat(raw_sim_metadata)
+    if len(sim_metadata) < 1:
+        return ""
 
-    for column_name in SIM_TABLE_COLUMNS:
-        if column_name not in prenight_df.reset_index().columns:
-            prenight_df[column_name] = "Unknown"
-
-    prenight_df = prenight_df.reset_index().loc[:, SIM_TABLE_COLUMNS]
+    sim_metadata.sort_values(by="creation_time", ascending=True, inplace=True)
+    prenight_df = sim_metadata.reset_index().loc[:, SIM_TABLE_COLUMNS]
 
     # "Plot"
     prenight_table = "#" + prenight_df.to_csv(sep="\t", index=False)
