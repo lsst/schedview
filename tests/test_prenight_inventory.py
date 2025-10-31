f6eabdf3d9b68792798a26480ce0db59891fef83
Eric Neilsen <neilsen@fnal.gov>
2025-10-31 13:45:34 -0700

    Update test of prenight_inventory for new dependency

--- tests/test_prenight_inventory.py
+++ tests/test_prenight_inventory.py
@@ -1,69 +1,88 @@
+import datetime
 import io
 import os
 import unittest
 from unittest.mock import patch
+from uuid import UUID
 
-import numpy as np
 import pandas as pd
+from pandas import Timestamp
 
 import schedview.app.prenight_inventory
 from schedview import DayObs
 
-TEST_ARCHIVE_SIM_METADATA = {
-    "s3://rubin:rubin-scheduler-prenight/opsim/2025-06-30/1/": {
-        "scheduler_version": "3.10.0",
-        "opsim_config_repository": "https://github.com/lsst-ts/ts_config_ocs",
-        "opsim_config_script": "ts_config_ocs/Scheduler/feature_scheduler/auxtel/fbs_spec_flex_survey.py",
-        "opsim_config_version": "v0.28.12-68-g4bf56c5",
-        "telescope": "auxtel",
-        "label": "Test auxtel entry",
-        "files": {
-            "environment": {"md5": "e8eb7cd0f1ec0278d4678421501c18c8", "name": "environment.txt"},
-            "observations": {"md5": "2f3be1c3ecdb7681e8372aa6c91f7344", "name": "opsim.db"},
+TEST_PRENIGHT_INDEX = pd.DataFrame(
+    [
+        {
+            "comments": {},
+            "conda_env_sha256": "",
+            "config_url": None,
+            "creation_time": Timestamp("2025-10-29 14:06:08.276173+0000", tz="UTC"),
+            "daily_id": 2,
+            "files": {
+                "rewards": "s3://rubin:rubin-scheduler-prenight/opsim/vseq/simonyi/2025-10-29/40b559a3-16a1-4004-9ed6-644cef98ea84/rewards.h5"
+            },
+            "first_day_obs": datetime.date(2025, 10, 29),
+            "last_day_obs": datetime.date(2025, 10, 31),
+            "parent_last_day_obs": datetime.date(2025, 10, 28),
+            "parent_visitseq_uuid": UUID("60a4a166-3dbc-4de0-8d38-d9933e243b9a"),
+            "scheduler_version": "v3.18.1",
+            "sim_creation_day_obs": datetime.date(2025, 10, 29),
+            "sim_runner_kwargs": None,
+            "tags": ["ideal", "nominal", "prenight"],
+            "telescope": "simonyi",
+            "visitseq_label": "Nominal start and overhead, ideal conditions, run at "
+            "2025-10-29T07:02:28-07:00",
+            "visitseq_url": "s3://rubin:rubin-scheduler-prenight/opsim/vseq/simonyi/2025-10-29/40b559a3-16a1-4004-9ed6-644cef98ea84/visits.h5",
+            "visitseq_uuid": UUID("60a4a166-3dbc-4de0-8d38-d9933e243b9b"),
         },
-        "tags": ["ideal", "nominal"],
-        "simulated_dates": {"first": "2025-06-30", "last": "2025-07-02"},
-    },
-    "s3://rubin:rubin-scheduler-prenight/opsim/2025-06-30/10/": {
-        "scheduler_version": "3.10.0",
-        "opsim_config_repository": "https://github.com/lsst-ts/ts_config_ocs",
-        "opsim_config_script": "ts_config_ocs/Scheduler/feature_scheduler/maintel/fbs_config_sv_survey.py",
-        "opsim_config_version": "v0.28.12-68-g4bf56c5",
-        "telescope": "simonyi",
-        "label": "test simonyi entry",
-        "files": {
-            "environment": {"md5": "e8eb7cd0f1ec0278d4678421501c18c8", "name": "environment.txt"},
-            "observations": {"md5": "2f3be1c3ecdb7681e8372aa6c91f7344", "name": "opsim.db"},
+        {
+            "comments": {},
+            "conda_env_sha256": "",
+            "config_url": None,
+            "creation_time": Timestamp("2025-10-29 14:12:06.321309+0000", tz="UTC"),
+            "daily_id": 3,
+            "files": {
+                "rewards": "s3://rubin:rubin-scheduler-prenight/opsim/vseq/simonyi/2025-10-29/d86a1d28-11df-47e1-9d4d-9268166563fa/rewards.h5"
+            },
+            "first_day_obs": datetime.date(2025, 10, 29),
+            "last_day_obs": datetime.date(2025, 10, 31),
+            "parent_last_day_obs": datetime.date(2025, 10, 28),
+            "parent_visitseq_uuid": UUID("60a4a166-3dbc-4de0-8d38-d9933e243b9a"),
+            "scheduler_version": "v3.18.1",
+            "sim_creation_day_obs": datetime.date(2025, 10, 29),
+            "sim_runner_kwargs": None,
+            "tags": ["delay_60", "ideal", "prenight"],
+            "telescope": "auxtel",
+            "visitseq_label": "Start time delayed by 60 minutes, nominal slew and visit "
+            "overhead, ideal conditions, run at "
+            "2025-10-29T07:06:57-07:00",
+            "visitseq_url": "s3://rubin:rubin-scheduler-prenight/opsim/vseq/simonyi/2025-10-29/d86a1d28-11df-47e1-9d4d-9268166563fa/visits.h5",
+            "visitseq_uuid": UUID("60a4a166-3dbc-4de0-8d38-d9933e243b9c"),
         },
-        "tags": ["ideal", "nominal"],
-        "simulated_dates": {"first": "2025-06-30", "last": "2025-07-02"},
-    },
-}
+    ]
+).set_index("visitseq_uuid")
 
 
 class TestPrenightInventory(unittest.TestCase):
 
     @patch(
-        "schedview.app.prenight_inventory.read_archived_sim_metadata",
+        "schedview.app.prenight_inventory.get_prenight_index",
         autospec=True,
     )
     def test_prenight_inventory(self, mock_read_archived_sim_metadata):
-        mock_read_archived_sim_metadata.return_value = TEST_ARCHIVE_SIM_METADATA
+        mock_read_archived_sim_metadata.return_value = TEST_PRENIGHT_INDEX
 
         # get_prneight_table complains if these are not set.
         os.environ["LSST_DISABLE_BUCKET_VALIDATION"] = "1"
         if "S3_ENDPOINT_URL" not in os.environ:
             os.environ["S3_ENDPOINT_URL"] = "dummy"
-        prenight_table = schedview.app.prenight_inventory.get_prenight_table(
-            DayObs.from_date("2025-06-30"), "s3://rubin:rubin-scheduler-prenight/opsim/", num_nights=1
-        )
+        prenight_table = schedview.app.prenight_inventory.get_prenight_table(DayObs.from_date("2025-10-29"))
 
         # make sure we can parse the output as a tab separated value string
         prenight_df = pd.read_csv(io.StringIO(prenight_table), sep="\t")
 
         # Is the data what we expect, given the fake data we fed it?
-        assert len(prenight_df) == 2
-        assert np.all(prenight_df.sim_execution_date == "2025-06-30")
-        assert len(prenight_df.query('telescope=="simonyi"')) == 1
-        assert len(prenight_df.query('telescope=="auxtel"')) == 1
+        assert len(prenight_df.query('telescope=="simonyi"')) == 2
+        assert len(prenight_df.query('telescope=="auxtel"')) == 2
         pass
