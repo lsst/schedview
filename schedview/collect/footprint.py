dcecac2974543780d9086a5eabcea0592e737edf
Eric Neilsen <neilsen@fnal.gov>
2025-11-05 14:16:30 -0800

    use python sum instead of np.sum to quiet warning from numpy

--- schedview/collect/footprint.py
+++ schedview/collect/footprint.py
@@ -8,7 +8,7 @@ def get_footprint(nside=32):
     # Load up a default footprint from rubin_scheduler
     sky_area_generator = CurrentAreaMap(nside=nside)
     band_footprints, _ = sky_area_generator.return_maps()
-    footprint = np.sum(band_footprints[b] for b in band_footprints.dtype.fields.keys())
+    footprint = sum(band_footprints[b] for b in band_footprints.dtype.fields.keys())
 
     footprint[footprint == 0] = np.nan
     return footprint
