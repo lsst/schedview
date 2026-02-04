22abe2ac0100b7f46b39cf0aa97b8e73af39f2c1
Eric Neilsen <neilsen@fnal.gov>
2026-02-04 14:40:32 -0800

    adding metric comptutation utils

--- schedview/compute/maf.py
+++ schedview/compute/maf.py
@@ -1,14 +1,17 @@
+import datetime
 import sqlite3
+from collections.abc import Mapping
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
+import numpy as np
 import pandas as pd
 from rubin_scheduler.scheduler.utils import SchemaConverter
 from rubin_sim import maf
 
 from schedview.util import band_column
 
-__all__ = ["compute_metric_by_visit"]
+__all__ = ["compute_metric_by_visit", "compute_scalar_metric_at_mjd", "compute_mixed_scalar_metric_at_mjd"]
 
 
 def _visits_to_opsim(visits, opsim):
@@ -42,7 +45,7 @@ def _visits_to_opsim(visits, opsim):
         merged_opsim.to_sql("observations", con)
 
 
-def compute_metric(visits, metric_bundle):
+def compute_metric(visits, metric_bundle, sqlite=True):
     """Compute metrics with MAF.
 
     Parameters
@@ -52,6 +55,10 @@ def compute_metric(visits, metric_bundle):
         database).
     metric_bundle : `maf.MetricBundle`, `dict`, or `list` of `maf.MetricBundle`
         The metric bundle(s) to run.
+    sqlite : `bool`
+        Write visits to an sqlite3 database and then read the visits back
+        from it when computing the metrics. Needed when metric bundles have
+        constraints.
 
     Returns
     -------
@@ -62,11 +69,15 @@ def compute_metric(visits, metric_bundle):
     metric_bundles = [metric_bundle] if passed_one_bundle else metric_bundle
 
     with TemporaryDirectory() as working_dir:
-        visits_db = Path(working_dir).joinpath("visits.db").as_posix()
-        _visits_to_opsim(visits, visits_db)
+        if sqlite:
+            visits_db = Path(working_dir).joinpath("visits.db").as_posix()
+            _visits_to_opsim(visits, visits_db)
 
-        bundle_group = maf.MetricBundleGroup(metric_bundles, visits_db, out_dir=working_dir)
-        bundle_group.run_all()
+            bundle_group = maf.MetricBundleGroup(metric_bundles, visits_db, out_dir=working_dir)
+            bundle_group.run_all()
+        else:
+            bundle_group = maf.MetricBundleGroup(metric_bundles, None, out_dir=working_dir)
+            bundle_group.run_current(None, visits.to_records(index=False))
 
     return metric_bundle
 
@@ -95,7 +106,10 @@ def compute_metric_by_visit(visits, metric, constraint=""):
     slicer = maf.OneDSlicer("observationId", bin_size=1)
     metric_bundle = maf.MetricBundle(slicer=slicer, metric=metric, constraint=constraint)
 
-    compute_metric(visits, metric_bundle)
+    # If the constraint is set, we need to use sqlite
+    use_sqlite = len(constraint) > 0
+
+    compute_metric(visits, metric_bundle, sqlite=use_sqlite)
     result = pd.Series(metric_bundle.metric_values, index=slicer.slice_points["bins"][:-1].astype(int))
     result.index.name = "observationId"
     return result
@@ -125,15 +139,82 @@ def compute_hpix_metric_in_bands(visits, metric, constraint="", nside=32):
     # Do only the filters we actually used
     used_bands = visits[band_column(visits)].unique()
 
+    # If the constraint is set, we need to use sqlite
+    use_sqlite = len(constraint) > 0
+
     bundles = {}
     for this_band in used_bands:
-        this_constraint = f"{band_column(visits)} == '{this_band}'"
-        if len(constraint) > 0:
-            this_constraint += f" AND {constraint}"
+        band_visits = visits.query(f"{band_column(visits)} == '{this_band}'")
         slicer = maf.HealpixSlicer(nside=nside, verbose=False)
-        bundles[this_band] = maf.MetricBundle(metric, slicer, this_constraint)
+        bundles[this_band] = maf.MetricBundle(metric, slicer, constraint)
+        compute_metric(band_visits, bundles[this_band], sqlite=use_sqlite)
 
-    compute_metric(visits, bundles)
     metric_values = {b: bundles[b].metric_values for b in bundles if bundles[b].metric_values is not None}
 
     return metric_values
+
+
+def compute_scalar_metric_at_mjd(
+    visits,
+    slicer,
+    metric,
+    mjd,
+    summary_metric=None,
+    run_name=None,
+    query="",
+    mjd_column="observationStartMJD",
+):
+    visits = visits.loc[visits[mjd_column] < mjd, :]
+
+    if len(query) > 0:
+        visits = visits.query(query)
+
+    if len(visits) == 0:
+        return np.nan
+
+    if run_name is None:
+        run_name = "Run" + datetime.datetime.now().isoformat()
+
+    if summary_metric is not None:
+        bundle = maf.MetricBundle(
+            metric,
+            slicer,
+            summary_metrics=[summary_metric],
+            run_name=run_name,
+        )
+    else:
+        bundle = maf.MetricBundle(
+            metric,
+            slicer,
+            run_name=run_name,
+        )
+
+    compute_metric(visits, bundle, sqlite=False)
+
+    if summary_metric is None:
+        metric_name = metric.name
+        metric_values = bundle.metric_values
+    else:
+        bundle.compute_summary_stats()
+        metric_name = summary_metric.name
+        metric_values = (
+            tuple(bundle.summary_values.values())
+            if isinstance(bundle.summary_values, Mapping)
+            else bundle.summary_values
+        )
+
+    assert len(metric_values) == 1
+    return {metric_name: metric_values[0]}
+
+
+def compute_mixed_scalar_metric_at_mjd(
+    start_visits, end_visits, transition_mjd, *args, mjd_column="observationStartMJD", **kwargs
+):
+    visits = pd.concat(
+        (
+            start_visits.loc[start_visits[mjd_column] <= transition_mjd, :].dropna(axis="columns", how="all"),
+            end_visits.loc[transition_mjd < end_visits[mjd_column], :].dropna(axis="columns", how="all"),
+        )
+    )
+    result = compute_scalar_metric_at_mjd(visits, *args, **kwargs)
+    return result
