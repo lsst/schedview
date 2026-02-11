c3312013a95db6511bc5cda11782a25343e7c44f
Lynne Jones <ljones.uw@gmail.com>
2026-02-10 20:52:28 -0800

    Move to black 2.61 and update requirements

--- schedview/__init__.py
+++ schedview/__init__.py
@@ -1,4 +1,11 @@
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("rubin_sim")
+except PackageNotFoundError:
+    # package is not installed
+    pass
+
 from .dayobs import DayObs
 from .sphere import *
 from .util import band_column
-from .version import __version__
